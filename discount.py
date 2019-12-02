15cf88e2254297fa8822d33e8f7d44906bdba7e3
vignesh-hub <58372359+vignesh-hub@users.noreply.github.com>
2019-12-02 12:53:50 +0530

    discount
    simple discount print example program

---
+++ discount.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 30 22:43:06 2019
+
+@author: DELL
+"""
+print("what is the cost?")
+n=input("enter the prize amount in INR\n")
+print("cost:",n)
+d=int(n)
+a=0.9*d
+print("discount amount is:",a)
